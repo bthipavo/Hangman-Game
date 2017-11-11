57477d2d0f6fde5729cc2c3d1f3dabbee92e4e69
bthipavo <bthipavo@gmail.com>
2017-11-10 19:11:24 -0800

    edits

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,36 +1,109 @@
 
-//capture key
-function hangMan(event){ 
-	console.log("in JS");
-	document.write("Hello World<br>");
-	// var letter = event.key;
-
+	var listofWords = ["One Direction", "New Kids on The Block", "Backstreet Boys", "Boyz II Men", "Jonas Brother", "EXO", "98 Degrees", "Big Bang", "Hanson", "Westlife", "The Jackson 5", "New Edition"];
+	var newGame = 0;
 	var currentWord = "";
-	currentWord = game.getWord();
-	document.write(currentWord + "<br>");
-	game.createLetters();
-	
-}
+	var guessArray = [];
+	var guessArrayCounter = 0;
+	var numberGuesses = 0;
+	var currentWordCount = 0;
+	var letters, blankInput;
 
-var game = {
-
-	listofWords: ["Backstreet Boys", "98 Degrees", "NSYNC"],
-
+//checks to see if there is a game going on
+	function checkGame() {
+		if (currentWord) newGame = 1;
+			else newGame = 0;
+		console.log("new game going on? " + newGame);
+		return newGame;
+	}
 //function randomly picks a word from the list//
-	getWord: function() {
-		console.log(this.listofWords[Math.floor(Math.random()*this.listofWords.length)])
-		// currentWord = this.listofWords[Math.floor(Math.random()*this.listofWords.length)];
-		return this.listofWords[Math.floor(Math.random()*this.listofWords.length)];
-;
-	},
+	function getWord() {
 
-	createLetters: function() {
-		for(i=0; i<currentWord.length; i++) {
-			document.write("_ ");
-		}
-	},
-
-	checkLetters: function() {
+		currentWord = this.listofWords[Math.floor(Math.random()*this.listofWords.length)];
+		currentWordCount = currentWord.replace(/\s+/g, "").length;
+		this.printBlanks();
+		console.log(currentWord + "current word count " + currentWordCount);
 
 	}
+
+	// createLetters: function() {
+	// 	for(i=0; i<currentWord.length; i++) {
+	// 		document.write("_ ");
+	// 	}
+	// },
+
+//check letters entered and puts them into array, also checks for dupes
+	function checkLetters(letter) {
+		console.log("checking letters");
+		var count = 0;
+		for (i = 0; i<guessArray.length; i++) {
+			if (letter == guessArray[i]) {
+				count++;
+			}
+			else {
+				count = count;
+			}
+		}
+		if (count > 0) {
+			guessArray = guessArray;
+			document.getElementById("errors").innerHTML=("letter has already been entered");
+		}
+		else {
+			guessArray.push(letter);
+			numberGuesses++;
+			this.printLetters();
+			this.correctGuess(letter);
+
+
+		}
+		document.getElementById("numGuesses").innerHTML = numberGuesses;
+		console.log(letter);
+		console.log(currentWord);
+	}
+
+	function correctGuess(letter) {
+		console.log("in correct guess function " + currentWord);
+
+		for (i = 0; i < currentWord.length; i++) {
+			if (letter.toUpperCase() == currentWord[i].toUpperCase()) {
+				document.getElementById("correctWord").innerHTML += currentWord[i].toUpperCase()  + " ";
+				currentWordCount--;
+				console.log(" current word count left " + currentWordCount);
+
+				if (currentWordCount == 0) {
+					document.getElementById("win").innerHTML = " yay you win!";
+		}
+			}
+		}
+
+	}
+
+	//print letters guessed
+
+	function printLetters() {
+		console.log(" counter guess " + (numberGuesses-1));
+		document.getElementById("ltrGuessed").innerHTML += guessArray[numberGuesses-1] + " ";
+
+	}
+
+	function printBlanks() {
+
+
+        var letter;
+        for (i = 0; i < currentWord.length; i++) {
+            if(currentWord.charAt(i) == ' ') {
+            	document.getElementById("letter").insertAdjacentHTML('beforeend', 'A');
+            }
+            else {
+            // letter = '<li class="letter letter' + currentWord.charAt(i).toUpperCase() + '">' + currentWord.charAt(i).toUpperCase() + '</li>';
+            document.getElementById("letter").insertAdjacentHTML('beforeend', '__ ');
+        }
+	}
 }
+	function remainingGuesses() {
+
+
+	} 
+
+	function gamesPlayed() {
+
+	}
