16cb39d90d7131fdaa1579d0d3c90ffc09cd6f32
bthipavo <bthipavo@gmail.com>
2017-11-11 01:29:26 -0800

    basics working need css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,75 @@
 
 	var listofWords = ["One Direction", "New Kids on The Block", "Backstreet Boys", "Boyz II Men", "Jonas Brother", "EXO", "98 Degrees", "Big Bang", "Hanson", "Westlife", "The Jackson 5", "New Edition"];
-	var newGame = 0;
+	var startGame = 0;
 	var currentWord = "";
 	var guessArray = [];
 	var guessArrayCounter = 0;
 	var numberGuesses = 0;
+	var numberLeft = 0;
 	var currentWordCount = 0;
-	var letters2, letters3, blankInput;
+	var numberWins = -1;
+	var numberLoss = 0;
+	var totalGuesses = 7;
+	var letters2, letters3, appendLetter;
+
+	function newGame() {
+		startGame = 1;
+		currentWord = "";
+		guessArray = [];
+		guessArrayCounter = 0;
+		numberGuesses = 0;
+		numberLeft = 0;
+		currentWordCount = 0;
+		totalGuesses = 7;
+		document.getElementById("win").innerHTML = "";
+		document.getElementById("letter").innerHTML = "";
+		// letters3 = "";
+		appendLetter = "";
+		document.getElementById("errors").innerHTML = "";
+		document.getElementById("ltrGuessed").innerHTML = "";
+		document.getElementById("numGuesses").innerHTML = "";
+		document.getElementById("numWin").innerHTML = numberWins;
+		document.getElementById("numLoss").innerHTML = numberLoss;
+		document.getElementById("guessLeft").innerHTML = totalGuesses;
+		this.getWord();
+
+	}
 
 //checks to see if there is a game going on
-	function checkGame() {
-		if (currentWord) newGame = 1;
-			else newGame = 0;
-		console.log("new game going on? " + newGame);
-		return newGame;
+	function checkGame(letter) {
+		if (currentWordCount > 0) checkLetters(letter);
+		if(currentWordCount == 0 && startGame == 0) {
+				numberWins++;
+				this.newGame();
+			}
+		if(currentWordCount == 0) {
+			startGame--;
+		}
+		if ((totalGuesses - numberLeft) == 0 && startGame ==0){
+			numberLoss++;
+			this.newGame();
+		}
+		if ((totalGuesses - numberLeft) == 0)
+			startGame--;
 	}
 //function randomly picks a word from the list//
 	function getWord() {
 
 		currentWord = this.listofWords[Math.floor(Math.random()*this.listofWords.length)];
-		currentWordCount = currentWord.replace(/\s+/g, "").length;
+		currentWordCount = (currentWord.replace(/\s+/g, "").length);
 		this.printBlanks();
 		console.log(currentWord + "current word count " + currentWordCount);
 
 	}
 
-	// createLetters: function() {
-	// 	for(i=0; i<currentWord.length; i++) {
-	// 		document.write("_ ");
-	// 	}
-	// },
 
 //check letters entered and puts them into array, also checks for dupes
 	function checkLetters(letter) {
-		console.log("checking letters");
+		// console.log("checking letters " + letter);
 		var count = 0;
-		for (i = 0; i<guessArray.length; i++) {
+		if(currentWordCount == 0)
+			startGame--;
+		for (var i = 0; i<guessArray.length; i++) {
 			if (letter == guessArray[i]) {
 				count++;
 			}
@@ -50,6 +84,7 @@
 		else {
 			guessArray.push(letter);
 			numberGuesses++;
+			document.getElementById("errors").innerHTML="";
 			this.printLetters();
 			this.correctGuess(letter);
 
@@ -61,19 +96,26 @@
 	}
 
 	function correctGuess(letter) {
-		console.log("in correct guess function " + currentWord);
-
-		for (i = 0; i < currentWord.length; i++) {
+		// console.log("in correct guess function " + currentWord);
+		var counter1 = 0;
+		// console.log("letter being type " + letter);
+		for (var i = 0; i < currentWord.length; i++) {
 			if (letter.toUpperCase() == currentWord[i].toUpperCase()) {
-				// (document.getElementById("letter"))[i].style.opacity = 1;
-				//document.getElementById("correctWord").innerHTML += currentWord[i].toUpperCase()  + " ";
 				currentWordCount--;
 				letters3[i].style.opacity = 1;
-
-				if (currentWordCount == 0) {
-					document.getElementById("win").innerHTML = " yay you win!";
-		}
+				console.log("word count " + currentWordCount);
 			}
+			else {
+				counter1++;
+				}
+			if (currentWordCount == 0) {
+				// numberWins++;
+				document.getElementById("win").innerHTML = " yay you win!";
+			}
+			if (counter1 >= currentWord.length) {
+				remainingGuesses();
+			}
+
 		}
 
 	}
@@ -81,41 +123,43 @@
 	//print letters guessed
 
 	function printLetters() {
-		console.log(" counter guess " + (numberGuesses-1));
+		// console.log(" counter guess " + (numberGuesses-1));
 		document.getElementById("ltrGuessed").innerHTML += guessArray[numberGuesses-1] + " ";
 
 	}
 
 	function printBlanks() {
 		letters2 = document.getElementById("letter");
-		var appendLetter;
+		
 		letters3 = letters2.children;
-		// letters3 = letters2.children;
-		// var appendLetter;
-        for (i = 0; i < currentWord.length; i++) {
-        	// letters2[i].style.opacity = 0.5;
-            // if(currentWord.charAt(i) == ' ') {
-            // 	document.getElementById("letter").insertAdjacentHTML('beforeend', 'A');
-            // }
-            // else {
-            // // letter = '<li class="letter letter' + currentWord.charAt(i).toUpperCase() + '">' + currentWord.charAt(i).toUpperCase() + '</li>';
+		// console.log(" current word length" + currentWord.length);
+        for (var i = 0; i < currentWord.length; i++) {
             appendLetter = ('<li>' + currentWord.charAt(i).toUpperCase() + '</li>');
             letters2.insertAdjacentHTML('beforeend', appendLetter);
-            letters3[i].style.opacity = 0.5;
+            letters3[i].style.opacity = 0;
 
         //}
 	}	
-	
 
-	console.log("letters3");
-	console.log(letters3);
-	console.log(letters2);
 }
 	function remainingGuesses() {
-
+		numberLeft++;
+		document.getElementById("guessLeft").innerHTML = (totalGuesses - numberLeft);
+		console.log(" number left " + (totalGuesses-numberLeft));
+		if ( (totalGuesses - numberLeft) <= 0) {
+			// numberLoss++;
+			console.log("number loss " +numberLoss);
+			document.getElementById("win").innerHTML = " you lose!";
+		}
 
 	} 
 
-	function gamesPlayed() {
+
+	function resetGame() {
+		numberWins = -1;
+		numberLoss = 0;
+		this.newGame();
 
 	}
+
+
